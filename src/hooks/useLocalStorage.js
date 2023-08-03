fdf6a947ce628cddec149253709f2693f2a126d8
Sharon Arana <xel.dev3@inxel.mx>
2023-08-02 18:08:05 -0600

    Manejo de error

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -24,6 +24,7 @@ function useLocalStorage(itemName, initialValue) {
       } catch (error) {
         setLoading(false);
         setError(true);
+        //console.log(error)
       }
     }, 2000);
   }, []);
