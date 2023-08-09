e5b87027d2dd77ea994aae1204d4e8eff6f212c2
Sharon Arana <xel.dev3@inxel.mx>
2023-08-08 19:04:11 -0600

    Estilos básicos de modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createPortal } from "react-dom";
 
+import "./Modal.css";
+
 function Modal({ children }) {
   return createPortal(
     <div className="Modal">{children}</div>,
